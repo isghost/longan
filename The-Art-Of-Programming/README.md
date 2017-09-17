8e093b2351b3e7182eb83f7c91cce5c215aef570
ccy <810278677@qq.com>
2017-09-17 15:07:14 +0800

    [upd]增加《编程之法》第一章第一节代码

---
+++ The-Art-Of-Programming/README.md
@@ -0,0 +1,3 @@
+### 关于此工程
+针对《编程之法：面试和算法心得》这本书的Java代码实现。    
+书本地址：[github](https://github.com/julycoding/The-Art-Of-Programming-By-July/blob/master/ebook/zh/Readme.md)
---
+++ The-Art-Of-Programming/src/main/java/Chapter1/Code01_01.java
@@ -0,0 +1,80 @@
+package Chapter1;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+/**
+ * Created by isghost on 2017/9/16.
+ * 原题目地址：https://github.com/julycoding/The-Art-Of-Programming-By-July/blob/master/ebook/zh/01.01.md
+ */
+public class Code01_01 {
+    /**
+     * 三步反转法
+     */
+    void reverseString(char[] str, int from, int to) {
+        while (from < to) {
+            char t = str[from];
+            str[from++] = str[to];
+            str[to--] = t;
+        }
+    }
+
+    /**
+     * 左移字符串
+     *
+     * @param str 需要移动的字符串
+     * @param m   要移动的个数
+     */
+    char[] leftRotateString1(char[] str, int m) {
+        char [] newStr = str.clone();
+        int n = newStr.length;
+        m %= n;
+        reverseString(newStr, 0, m - 1);
+        reverseString(newStr, m, n - 1);
+        reverseString(newStr, 0, n - 1);
+        return newStr;
+    }
+
+    @Test
+    public void resolve1() {
+        char actuals[] = "abc123456789".toCharArray();
+        int m = 3;
+        char expecteds2[] = "123456789abc".toCharArray();
+        assertArrayEquals(expecteds2, leftRotateString1(actuals, m));
+    }
+    //------------------------- 分割线 ------------------------------------------
+
+    /**
+     * 直接平移法（我的方法）
+     * 根据平移数量，算出每个字符的新位置，直接移动过去
+     */
+    char [] leftRotateString2(char[] str, int m) {
+        char [] newStr = str.clone();
+        int n = newStr.length;
+        for(int i = 0;i<m;i++){
+            int index = i;
+            char tmpChar = newStr[index];
+            do {
+                // 将nextIndex上的字符移动到index
+                int nextIndex = (index + m + n) % n;
+                if (nextIndex == i) {
+                    newStr[index] = tmpChar;
+                } else {
+                    newStr[index] = newStr[nextIndex];
+                }
+                index = nextIndex;
+            } while (index != i);
+        }
+        return newStr;
+
+    }
+
+    @Test
+    public void resolve2() {
+        char actuals[] = "abc123456789".toCharArray();
+        int m = 3;
+        char expecteds2[] = "123456789abc".toCharArray();
+        assertArrayEquals(expecteds2, leftRotateString2(actuals, m));
+    }
+}
